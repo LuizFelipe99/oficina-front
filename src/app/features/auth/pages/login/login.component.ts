61fdb61a94026a9286cc6fe23cd71964cee2f3ab
Luiz Abreu <luizfelipetomaz30@gmail.com>
2026-04-27 19:26:08 -0300

    feat: implementação de login com integração à API e armazenamento de token

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [FormsModule],
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        this.authService.saveToken(response.token);
+        console.log('Login realizado com sucesso');
+      },
+      error: (err) => {
+        console.error('Erro no login', err);
+      }
+    });
+  }
 }
